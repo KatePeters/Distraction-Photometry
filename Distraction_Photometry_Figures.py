80e430ba93fd961fbd2490c78f483b41ac53959c
KP <kp259@le.ac.uk>
2018-02-05 19:53:58 +0000

    Created figures script
    Created a script to generate and save figures for the distraction photometry paper, added some basic comments about what will be included

---
+++ Distraction_Photometry_Figures.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 19:23:37 2018
+
+@author: u1490431
+"""
+
+''' New analysis file for distraction photometry paper, for generating figures
+    specific to the paper and adding titles and information to them 
+
+'''
+
+# Imports
+
+# Data processing functions 
+
+
+# Figure 1
+# Long time course figure, lick day 
+# Long time course distraction day, with markers added for distractors (in illustrator?) 
+
+
+# Figure 2
+# Averaged distracted vs not sidtracted (photometry signals snipped)
+
+# Figure 3
+# Mean peak height, distracted vs not distracted (VTA)
